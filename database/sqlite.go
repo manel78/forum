e62fc82cd8f50ba2c7911e7c1c72d01f8e6a4c6f
emanel <manel.elhadi@ynov.com>
2024-05-11 23:16:31 +0200

    test

---
+++ database/sqlite.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	// ouvre connection a la base de donées
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// table des utilisateurs
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY,
+		username TEXT UNIQUE,
+		password TEXT
+	)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// categories
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS categories (
+		id INTEGER PRIMARY KEY,
+		name TEXT UNIQUE
+	)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// messages
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS messages (
+		id INTEGER PRIMARY KEY,
+		user_id INTEGER,
+		category_id INTEGER,
+		content TEXT,
+		FOREIGN KEY (user_id) REFERENCES users(id),
+		FOREIGN KEY (category_id) REFERENCES categories(id)
+	)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Tables créées avec succès !")
+}
